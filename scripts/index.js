a5ea4e346ea454993d311e19b75556970f61ae4b
Reganzoh <57925655+Reganzoh@users.noreply.github.com>
2020-09-17 10:51:56 +1200

    commit

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("input");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+    return input.nodeValue.length;
+}
+
+function createListElement() {
+    var li = document.createListElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+
+    li.addEventListener("click", function () {
+        //creates a boolean that toggles the done class on li:
+        // if the list item is clicked this toggels the done class
+        var finished = this.classList.toggle("done"); 
+        // creates a remove button fot the finished item:
+        var removeButton = document.createElement("button");
+        removeButton.classList.add("deleteButton");
+        
+        //if the item is clicked (li add event listner) then 
+        // finished is true
+        
+    }
+    )
+}
